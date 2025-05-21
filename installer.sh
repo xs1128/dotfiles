734dc0e7cad564a1fd65c4619eb111da76818c0e
Xinsheng Ooi <84843079+xs1128@users.noreply.github.com>
2025-05-21 15:04:02 +0800

    Test signing key

--- installer.sh
+++ installer.sh
@@ -3,5 +3,5 @@
 files="bash_profile tmux.conf vimrc zprofile zshrc gitignore aliases vim gitconfig"
 
 for file in $files; do 
-    ln -s ~/.dotfiles/.$file ~/.$file
+    ln -s ~/.dotfiles/."$file" ~/."$file"
 done
